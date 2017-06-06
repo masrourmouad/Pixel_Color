3ec4e59fb261ae4e35754c598f24b29f50c463a8
masrourmouad <masrourmouad@yahoo.fr>
2017-06-07 00:40:39 +0100

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,38 @@
+
+
+local dyes = {
+	{"white",      "White",      "basecolor_white"},
+	{"grey",       "Grey",       "basecolor_grey"},
+	{"black",      "Black",      "basecolor_black"},
+	{"red",        "Red",        "basecolor_red"},
+	{"yellow",     "Yellow",     "basecolor_yellow"},
+	{"green",      "Green",      "basecolor_green"},
+	{"cyan",       "Cyan",       "basecolor_cyan"},
+	{"blue",       "Blue",       "basecolor_blue"},
+	{"magenta",    "Magenta",    "basecolor_magenta"},
+	{"orange",     "Orange",     "excolor_orange"},
+	{"violet",     "Violet",     "excolor_violet"},
+	{"brown",      "Brown",      "unicolor_dark_orange"},
+	{"pink",       "Pink",       "unicolor_light_red"},
+	{"light_brown","Light Brown","unicolor_light_brown"},
+	{"dark_grey",  "Dark Grey",  "unicolor_darkgrey"},
+	{"dark_green", "Dark Green", "unicolor_dark_green"},
+	{"royal_blue", "Royal Blue", "unicolor_royal_blue"},
+	{"lime_green", "Lime Green", "unicolor_lime_green"},
+}
+
+for i = 1, #dyes do
+	local name, desc, craft_color_group = unpack(dyes[i])
+
+	minetest.register_node("pixel_color:" .. name, {
+		description = desc .. " Pixel",
+		tiles = {"pixel_" .. name .. ".png"},
+		is_ground_content = false,
+		groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 3,
+				flammable = 3, wool = 1},
+		sounds = default.node_sound_defaults(),
+	})
+
+end
+
+
